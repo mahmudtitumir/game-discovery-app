bf6c08aee2c1c82734a6965a46b2078af7af3bd0
mahmudtitumir <titumir947@gmail.com>
2025-09-25 23:40:29 +0600

    Use Grid layout from Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css';
-import { Button, HStack } from '@chakra-ui/react';
+import { Grid, GridItem, Text } from '@chakra-ui/react';
 
 function App() {
     return (
         <>
-            <HStack>
-                <Button>Click me</Button>
-                <Button>Click me</Button>
-            </HStack>
+            <Grid templateColumns="200px repeat(2, 1fr)" gap={4}>
+                <GridItem colSpan={3} background={'blue.100'}>
+                    <Text>header</Text>
+                </GridItem>
+                <GridItem background={'blue.500'}>
+                    <Text>sidebar</Text>
+                </GridItem>
+                <GridItem background={'blue.1900'}>
+                    <Text>main</Text>
+                </GridItem>
+            </Grid>
         </>
     );
 }
