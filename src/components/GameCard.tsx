498c9fdc42eefb89d7a823fc75ce8c23d82955e9
mahmudtitumir <titumir947@gmail.com>
2025-09-28 12:03:13 +0600

    feat: enhance GameCard and GameGrid components with new features and styling; add PlatformIconList and CriticScore components

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,8 @@
-import { Card, Image, Text } from '@chakra-ui/react';
+import { Card, HStack, Image } from '@chakra-ui/react';
 import type { Game } from '../hooks/useUsers';
+import PlatformIconList from './PlatformIconList';
+import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
     game: Game;
@@ -8,21 +11,19 @@ interface Props {
 const GameCard = ({ game }: Props) => {
     return (
         <Card.Root maxW="sm" overflow="hidden">
-            <Image src={game.background_image} />
+            <Image src={getCroppedImageUrl(game.background_image)} />
             <Card.Body gap="2">
                 <Card.Title>{game.name}</Card.Title>
                 <Card.Description>
                     This sofa is perfect for modern tropical spaces, baroque
                     inspired spaces.
                 </Card.Description>
-                <Text
-                    textStyle="2xl"
-                    fontWeight="medium"
-                    letterSpacing="tight"
-                    mt="2"
-                >
-                    $450
-                </Text>
+                <HStack justifyContent="space-between">
+                    <PlatformIconList
+                        platforms={game.parent_platforms.map(p => p.platform)}
+                    />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
             </Card.Body>
         </Card.Root>
     );
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,45 @@
+import { HStack, Icon } from '@chakra-ui/react';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import type { IconType } from 'react-icons/lib';
+import {
+    FaWindows,
+    FaPlaystation,
+    FaXbox,
+    FaApple,
+    FaLinux,
+    FaAndroid,
+} from 'react-icons/fa';
+
+import type { Platform } from '../hooks/useUsers';
+
+interface Props {
+    platforms: Platform[];
+}
+const PlatformIconList = ({ platforms = [] }: Props) => {
+    const iconMap: { [key: string]: IconType } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        mac: FaApple,
+        nintendo: SiNintendo,
+        linux: FaLinux,
+        android: FaAndroid,
+        ios: MdPhoneIphone,
+        web: BsGlobe,
+    };
+    return (
+        <HStack>
+            {platforms.map(platform => (
+                <Icon
+                    key={platform.id}
+                    as={iconMap[platform.slug]}
+                    color="blue.500"
+                />
+            ))}
+        </HStack>
+    );
+};
+
+export default PlatformIconList;
