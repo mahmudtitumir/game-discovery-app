a528a1275cba8944a94834350fa8ac77eb0f3853
mahmudtitumir <titumir947@gmail.com>
2025-10-01 20:51:04 +0600

    feat: implement genres data and update useGenres hook to return static genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,25 +14,23 @@ const GameGrid = ({ gameQuery }: Props) => {
     const skeletors = [1, 2, 3, 4, 5, 6, 7, 8];
     if (error) return <Text>{error}</Text>;
     return (
-        <>
-            <SimpleGrid
-                columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-                padding="10px"
-                gap={6}
-            >
-                {isLoading &&
-                    skeletors.map(skeletor => (
-                        <GameCardContainer key={skeletor}>
-                            <GameCardSkeleton />
-                        </GameCardContainer>
-                    ))}
-                {data.map(game => (
-                    <GameCardContainer key={game.id}>
-                        <GameCard game={game} />
+        <SimpleGrid
+            columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+            padding="10px"
+            gap={6}
+        >
+            {isLoading &&
+                skeletors.map(skeletor => (
+                    <GameCardContainer key={skeletor}>
+                        <GameCardSkeleton />
                     </GameCardContainer>
                 ))}
-            </SimpleGrid>
-        </>
+            {data.map(game => (
+                <GameCardContainer key={game.id}>
+                    <GameCard game={game} />
+                </GameCardContainer>
+            ))}
+        </SimpleGrid>
     );
 };
 
