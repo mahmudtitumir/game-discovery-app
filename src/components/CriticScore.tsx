a2d7f30f4f7d5abe8fd722f43375509c8e357e8a
mahmudtitumir <titumir947@gmail.com>
2025-10-01 19:52:09 +0600

    fix: correct colorScheme to colorPalette in CriticScore; adjust gap in GameGrid; enhance GenreList layout and styling

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -8,7 +8,7 @@ const CriticScore = ({ score }: Props) => {
     const color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
     return (
         <Badge
-            colorScheme={color}
+            colorPalette={color}
             fontSize="14px"
             paddingX={3}
             borderRadius="4px"
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,11 @@
-import { HStack, Image, List, Spinner, Button } from '@chakra-ui/react';
+import {
+    HStack,
+    Image,
+    List,
+    Spinner,
+    Button,
+    Heading,
+} from '@chakra-ui/react';
 import useGenres, { type Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
@@ -14,36 +21,41 @@ const GenreList = ({ onSeletedGenre, selectedGenre }: Props) => {
     if (isLoading) return <Spinner size={'md'} />;
 
     return (
-        <List.Root listStyle="none">
-            {data.map(genre => (
-                <List.Item key={genre.id} paddingY="5px">
-                    <HStack>
-                        <Image
-                            boxSize="32px"
-                            borderRadius="6px"
-                            src={getCroppedImageUrl(genre.image_background)}
-                        />
-                        <Button
-                            size={'lg'}
-                            variant="plain"
-                            onClick={() => onSeletedGenre(genre)}
-                            textDecor={
-                                selectedGenre?.id === genre.id
-                                    ? 'underline'
-                                    : 'none'
-                            }
-                            fontWeight={
-                                selectedGenre?.id === genre.id
-                                    ? 'bold'
-                                    : 'normal'
-                            }
-                        >
-                            {genre.name}
-                        </Button>
-                    </HStack>
-                </List.Item>
-            ))}
-        </List.Root>
+        <>
+            <Heading>Genres</Heading>
+            <List.Root listStyle="none">
+                {data.map(genre => (
+                    <List.Item key={genre.id} paddingY="5px">
+                        <HStack>
+                            <Image
+                                boxSize="32px"
+                                borderRadius="6px"
+                                objectFit="cover"
+                                src={getCroppedImageUrl(genre.image_background)}
+                            />
+                            <Button
+                                variant="plain"
+                                textAlign="left"
+                                whiteSpace="nowrap"
+                                onClick={() => onSeletedGenre(genre)}
+                                textDecoration={
+                                    selectedGenre?.id === genre.id
+                                        ? 'underline'
+                                        : 'none'
+                                }
+                                fontWeight={
+                                    selectedGenre?.id === genre.id
+                                        ? 'bold'
+                                        : 'normal'
+                                }
+                            >
+                                {genre.name}
+                            </Button>
+                        </HStack>
+                    </List.Item>
+                ))}
+            </List.Root>
+        </>
     );
 };
 
