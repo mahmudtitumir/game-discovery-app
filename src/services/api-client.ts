f2063da4ca8e09765de29841e7088301076c1e0a
mahmudtitumir <titumir947@gmail.com>
2025-10-07 16:43:53 +0600

    refactor: update APIClient usage in useGames, useGenres, and usePlatforms hooks to streamline data fetching

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,15 +1,27 @@
-import axios from 'axios';
+import axios, { type AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
     count: number;
     results: T[];
 }
 
-const apiClient = axios.create({
+const axiosInstance = axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: 'c11f1dd443054e4eb536a2e316f06029',
     },
 });
 
-export default apiClient;
+class APIClient<T> {
+    endpoint: string;
+    constructor(endpoint: string) {
+        this.endpoint = endpoint;
+    }
+    getAll = (config: AxiosRequestConfig) => {
+        return axiosInstance
+            .get<FetchResponse<T>>(this.endpoint, config)
+            .then(res => res.data);
+    };
+}
+
+export default APIClient;
